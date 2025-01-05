bb48f21ffe0166e0a2b5c433720fad4fb52eab57
Ruhollah <ruhollahh01@gmail.com>
2025-01-05 13:33:50 +0330

    implement read loop

--- repl.go
+++ repl.go
@@ -1,6 +1,26 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokx > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
+}
 
 func cleanInput(input string) []string {
 	lowerCased := strings.ToLower(input)
